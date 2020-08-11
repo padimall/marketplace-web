6fa0aaf49e7abb8a19e307fc886fc254b51d2e12
rickyjulpiter <rickyjulpiter14@gmail.com>
2020-08-11 09:55:49 +0700

    front template

---
+++ demo/template/script.php
@@ -0,0 +1,22 @@
+<!-- latest jquery-->
+<script src="assets/js/jquery-3.3.1.min.js"></script>
+
+<!-- popper js-->
+<script src="assets/js/popper.min.js"></script>
+
+<!-- Bootstrap js-->
+<script src="assets/js/bootstrap.js"></script>
+
+<!-- Bootstrap js-->
+<script src="assets/js/bootstrap-notify.min.js"></script>
+
+<!-- Timer js-->
+<script src="assets/js/menu.js"></script>
+
+<!-- slick js-->
+<script src="assets/js/slick.js"></script>
+
+<!-- Theme js-->
+<script src="assets/js/modal.js"></script>
+<script src="assets/js/slider-animat-one.js"></script>
+<script src="assets/js/script.js"></script>
