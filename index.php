98f3f8a2e22830ea7c41d9e6d2fefe9b5fa25af1
rickyjulpiter <rickyjulpiter14@gmail.com>
2020-09-11 01:54:52 +0700

    coming soon landing page

--- index.php
+++ index.php
@@ -1,50 +1,59 @@
-<?php
-include('controller/product.php');
-$productView = new Product();
-$dataProduct = $productView->view_product();
-
-//check apakah mendapat return nilai
-if ($dataProduct[0] == 1) {
-    $dataProduct = $dataProduct[1];
-} else {
-    //trigger warning nantinya
-    $warning = 1;
+<!DOCTYPE html>
+<html>
+<style>
+body,
+html {
+    height: 100%;
+    margin: 0;
 }
 
-?>
-<!DOCTYPE html>
-<html lang="en">
+.bgimg {
+    background-image: url('https://www.lefthudson.com/wp-content/uploads/2019/11/white-wallpaper-1920x1080-unique-black-and-white-abstract-backgrounds-57-images-combination-of-white-wallpaper-1920x1080.jpg');
+    height: 100%;
+    background-position: center;
+    background-size: cover;
+    position: relative;
+    color: black;
+    font-family: "Courier New", Courier, monospace;
+    font-size: 25px;
+}
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
+.topleft {
+    position: absolute;
+    top: 0;
+    left: 16px;
+}
+
+.bottomleft {
+    position: absolute;
+    bottom: 0;
+    left: 16px;
+}
+
+.middle {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+}
+
+hr {
+    margin: auto;
+    width: 40%;
+}
+</style>
 
 <body>
-    <table style="border: 1px solid black;">
-        <tr>
-            <th>No</th>
-            <th>Nama Produk</th>
-            <th>Price</th>
-            <th>Description</th>
-            <th>Category</th>
-            <th>Stock</th>
-        </tr>
-        <?php
-        $no = 1;
-        foreach ($dataProduct as $row) {
-        ?>
-        <tr>
-            <td><?= $no++ ?></td>
-            <td><?= $row['name'] ?></td>
-            <td><?= $row['price'] ?></td>
-            <td><?= $row['description'] ?></td>
-            <td><?= $row['category'] ?></td>
-            <td><?= $row['stock'] ?></td>
-        </tr>
-        <?php } ?>
-    </table>
+
+    <div class="bgimg">
+        <div class="middle">
+            <h1>COMING SOON</h1>
+            <hr>
+            <p>Developing Stage</p>
+        </div>
+    </div>
+
 </body>
 
 </html>
